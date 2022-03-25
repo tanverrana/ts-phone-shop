e31eeff10a629e9a593652e3203895a2a9dc4837
tanverrana <tanversobur@gmail.com>
2022-03-26 00:48:24 +0600

    icon added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,20 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import "./Products.css";
 
 const Products = (props) => {
-    const { image, phone_name, price, brand } = props.product
+    const { image, phone_name, price, brand } = props.product;
+    const { handleAddToCart } = props;
     return (
         <div className="product">
             <img src={image} alt="" />
             <h4>Phone Name:{phone_name}</h4>
             <h5>Brand Name:{brand}</h5>
             <h5>Price:${price}</h5>
-            <button className="btn">
+            <button onClick={() => handleAddToCart(props.product)} className="btn">
                 Add To Cart
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,11 +6,18 @@ import "./Shop.css"
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [summary, setSummary] = useState([]);
     useEffect(() => {
         fetch("products.json")
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, []);
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+        const newSummary = [...summary, product];
+        setSummary(newSummary);
+    }
     return (
         <div className="shop-container">
             <div className="products-container">
@@ -18,12 +25,17 @@ const Shop = () => {
                     products.map(product => <Products
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     >
                     </Products>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <div>
+                    <img src={summary.image} alt="" />
+                    <p>summary:{summary.phone_name}</p>
+                </div>
             </div>
         </div>
     );
