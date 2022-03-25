a94e3a01e356f33e292953096708a7680f63fa8d
tanverrana <tanversobur@gmail.com>
2022-03-25 21:53:35 +0600

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>TS Phone Shop</h1>
+        </div>
+    );
+};
+
+export default Header;
