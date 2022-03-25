eafc0f8c10d60167f7da11076b8960a6473250c9
tanverrana <tanversobur@gmail.com>
2022-03-25 22:52:53 +0600

    create data

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Shop.css"
+
+const Shop = () => {
+    return (
+        <div className="shop-container">
+            <div className="products-container">
+                <h3>This is for Products</h3>
+            </div>
+            <div className="cart-container">
+                <h4>Order Summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
