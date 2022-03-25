45a2517725c50371fe13caf56080216b1b15a98c
tanverrana <tanversobur@gmail.com>
2022-03-25 23:13:40 +0600

    fetch data and get data

---
+++ src/components/Products/Products.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Products.css";
+
+const Products = (props) => {
+    const { image, phone_name, price, brand } = props.product
+    return (
+        <div className="product">
+            <img src={image} alt="" />
+            <h4>Phone Name:{phone_name}</h4>
+            <h5>Brand Name:{brand}</h5>
+            <h5>Price:{price}</h5>
+        </div>
+    );
+};
+
+export default Products;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Products from '../Products/Products';
 import "./Shop.css"
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("products.json")
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div className="shop-container">
             <div className="products-container">
-                <h3>This is for Products</h3>
+                {
+                    products.map(product => <Products
+                        key={product.id}
+                        product={product}
+                    >
+                    </Products>)
+                }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
