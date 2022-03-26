89c03ba5f49b6f8eb91d58e83cdba29be97e7e6d
tanverrana <tanversobur@gmail.com>
2022-03-26 09:42:29 +0600

    show product name

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import "./Summary.css";
 
 const Summary = (props) => {
     const { summary } = props;
+    console.log(summary);
+    const name = summary.map(product => product.phone_name);
     return (
         <div>
-            <h4>Order Summary</h4>
-            <h5>Summary:{summary.image}</h5>
+            <h2>Order Summary</h2>
+            <h5 className="phone">Phone Name:{name}</h5>
+            <button className="chose-btn">Chose One For Me</button>
+            <button className="remove-btn">Chose Again</button>
         </div>
     );
 };
